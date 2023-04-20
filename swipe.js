e15c967d253f3884f32a63ddf4737b33fe4c240a
Umut CAN <78921017+C1N-S4@users.noreply.github.com>
2023-04-20 12:45:01 +0300

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,8 +1,14 @@
 var clickCount = 0;
+var limit = 10; // Limit
 
-setInterval(function() {
+var intervalId = setInterval(function() {
   var element = document.querySelector("button[class='button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a']");
   element.click();
   clickCount++;
   console.log("Number of clicks: " + clickCount);
+
+  if (clickCount >= limit) {
+    clearInterval(intervalId);
+    console.log("Click limit reached, stopping interval.");
+  }
 }, 1000);
