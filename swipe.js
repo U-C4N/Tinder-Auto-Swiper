fa07738ac8140ccfac6c80159cbaf689b6b8b9ca
Umut CAN <78921017+C1N-S4@users.noreply.github.com>
2024-01-26 11:18:58 +0300

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,14 +1,19 @@
 var clickCount = 0;
-var limit = 10; // Limit
+var limit = prompt("Lütfen beğeni limiti girin:", 10); 
 
+var startTime = new Date();
 var intervalId = setInterval(function() {
   var element = document.querySelector("button[class='button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a']");
-  element.click();
-  clickCount++;
-  console.log("Number of clicks: " + clickCount);
+  if (element) {
+    element.click();
+    clickCount++;
+    console.log("Number of clicks: " + clickCount);
+  }
 
   if (clickCount >= limit) {
     clearInterval(intervalId);
-    console.log("Click limit reached, stopping interval.");
+    var endTime = new Date(); 
+    var timeSpent = (endTime - startTime) / 1000; 
+    alert("Beğeni limitine ulaşıldı: " + clickCount + " beğeni\nGeçen süre: " + timeSpent + " saniye");
   }
 }, 1000);
